c6744c81d15aec81d806552c979d4ef95e4612a7
davidlai <david.lai@evermight.com>
2017-03-07 13:37:02 -0500

    added users controller

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using PostsDataAccess;
+
+namespace CopProjectAPI.Controllers
+{
+    public class UsersController : ApiController
+    {
+        public IEnumerable<User> Get()
+        {
+            using (CopProjectDBEntities entities = new CopProjectDBEntities())
+            {
+                entities.Configuration.ProxyCreationEnabled = false;
+                return entities.Users.ToList();
+            }
+
+        }
+
+        public User Get(int orgId, int badge)
+        {
+            using (CopProjectDBEntities entities = new CopProjectDBEntities())
+            {
+                entities.Configuration.ProxyCreationEnabled = false;
+                return entities.Users.FirstOrDefault(obj => obj.Badge == badge && obj.OrgID == orgId);
+            }
+
+        }
+    }
+}
